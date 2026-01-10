33ebd8ccc0a5e145428e28531aac13118de9591f
imochoa <18612100+imochoa@users.noreply.github.com>
2026-01-10 20:45:36 +0100

    added tests

--- tutorials/running-man.nvim/scripts/minimal_init.lua
+++ tutorials/running-man.nvim/scripts/minimal_init.lua
@@ -6,14 +6,25 @@ print(filename)
 
 print("Setting runtimepaths...")
 -- local buf_path = vim.api.nvim_buf_get_name(0)
-local bufpath = vim.fn.expand("%:p")
 local bufdir = vim.fn.expand("%:p:h")
 local repodir = vim.fn.expand("%:p:h:h")
 
 vim.opt.rtp:append(bufdir)
 vim.opt.rtp:append(repodir)
-vim.opt.rtp:append(repodir .. "/plugin")
+vim.opt.rtp:append(repodir .. "plugin")
+-- vim.cmd([[let &rtp.=','.getcwd()]])
 
 -- runtime! plugin/plenary.vim
 -- vim.cmd(string.format("runtime! %s/plugin/loadit.lua", repodir))
-vim.cmd(string.format(":luafile %s/plugin/loadit.lua", repodir))
+-- vim.cmd(string.format(":luafile %s/plugin/loadit.lua", repodir))
+
+-- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
+if #vim.api.nvim_list_uis() == 0 then
+  print("no UI! set up tests...")
+  -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
+  -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
+  -- vim.cmd("set rtp+=deps/mini.nvim")
+  vim.opt.rtp:append(repodir .. "deps/mini.nvim")
+
+  require("mini.test").setup()
+end
