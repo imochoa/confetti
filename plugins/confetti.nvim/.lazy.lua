90a4b9cef7099e1567e7c4145897802305f9d821
imochoa <18612100+imochoa@users.noreply.github.com>
2026-01-11 18:26:23 +0100

    cleaned up confetti

--- plugins/confetti.nvim/.lazy.lua
+++ plugins/confetti.nvim/.lazy.lua
@@ -1,7 +1,58 @@
--- https://github.com/DrKJeff16/wezterm-types
--- https://github.com/folke/lazydev.nvim
--- still have to add  ---@type Config
--- not luarocks install though?
 return {
-	{ dir = "/Users/imochoa/Code/neovim-plugins/confetti.nvim" },
+	{
+		dir = "/Users/imochoa/Code/neovim-plugins/plugins/confetti.nvim",
+		-- config = function()
+		-- 	-- require("confetti")
+		-- 	require("confetti").setup()
+		-- end,
+		--
+		opts = {
+			reused_hlgroups = {},
+			colors = {
+				{
+					guifg = "black",
+					guibg = "white",
+					altfont = false,
+					bold = false,
+					inverse = false, -- Inverse will flip fg and bg
+					italic = false,
+					nocombine = false,
+					standout = false,
+					strikethrough = false,
+					undercurl = false,
+					underdashed = false,
+					underdotted = false,
+					underdouble = false,
+					underline = false,
+				},
+				{ guifg = "black", guibg = "magenta", altfont = true },
+				{ guifg = "black", guibg = "lime", bold = true },
+				{ guifg = "black", guibg = "yellow", italic = true },
+				{ guifg = "black", guibg = "red", nocombine = true },
+				{ guifg = "black", guibg = "darkviolet", standout = true },
+				{ guifg = "black", guibg = "chocolate", strikethrough = true },
+				{ guifg = "black", guibg = "thistle", undercurl = true },
+				{ guifg = "black", guibg = "orangered", underdashed = true },
+				{ guifg = "black", guibg = "greenyellow", underdotted = true },
+				{ guifg = "black", guibg = "acqua", underdouble = true },
+				{ guifg = "black", guibg = "hotpink", underline = true },
+			},
+		},
+		keys = {
+			{
+				"<leader>*",
+				function()
+					require("confetti").highlight_at_cursor()
+				end,
+				desc = "Highlight at cursor",
+			},
+			{
+				"<leader>**",
+				function()
+					require("confetti").clear_highlights()
+				end,
+				desc = "Clear all confetti highlights",
+			},
+		},
+	},
 }
--- plugins/confetti.nvim/dev.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		dir = "/Users/imochoa/Code/neovim-plugins/confetti.nvim",
-		-- config = function()
-		-- 	require("confetti")
-		-- 	-- require("confetti").setup()
-		-- end,
-	},
-}
